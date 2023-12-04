0871ab9f9e4015c6f1ff71699b1328de192cb6de
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2023-12-03 22:28:31 -0500

    arreglar controller rfc3

--- demo/src/main/java/com/example/demo/controller/ReservaController.java
+++ demo/src/main/java/com/example/demo/controller/ReservaController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.modelo.Consumo;
 import com.example.demo.modelo.Habitacion;
